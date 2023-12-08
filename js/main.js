d7c22dfec41f75d83e813f2135551373b3457dcd
nicomanunta <nic.manun@gmail.com>
2023-12-08 12:02:12 +0100

    Milestone 1 completato

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const images = [
     {
         image: 'img/01.webp',
-        title: 'Marvel\'s Spiderman Miles Morale',
+        title: "Marvel's Spiderman Miles Morale",
         text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
     }, {
         image: 'img/02.webp',
@@ -18,6 +18,71 @@ const images = [
     }, {
         image: 'img/05.webp',
         title: "Marvel's Avengers",
-        text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+        text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
     }
 ];
+
+// MILESTONE 1 cliccando sui pulsanti va avanti o indietro. All'ultima immagine ed alla prima non va più avanti
+
+let immagineDisplay = ""
+
+images.forEach(element => {
+    immagineDisplay += `<div id="item" class="item">
+                        <img src="./${element.image}" alt="">
+                        <div class="position-absolute bottom-50 text-right color-white padding-text">
+                            <h2 id="title">${element.title}</h2>
+                            <h5 id="text">${element.text}</h5>
+                        </div>
+                    </div> ` 
+    
+  
+});
+document.querySelector(".images").innerHTML = immagineDisplay
+
+let activeElement = 0
+let immagineVisualizzata = document.getElementsByClassName("item");
+immagineVisualizzata[activeElement].classList.add("active")
+
+
+document.querySelector(".next").addEventListener("click", function(){
+
+    immagineVisualizzata[activeElement].classList.remove("active")
+    activeElement ++
+    immagineVisualizzata[activeElement].classList.add("active")
+
+
+});
+document.querySelector(".prev").addEventListener("click", function(){
+    immagineVisualizzata[activeElement].classList.remove("active")
+    activeElement --
+    immagineVisualizzata[activeElement].classList.add("active")
+});
+
+
+
+
+
+
+
+
+// let item = document.getElementById("item")
+// document.querySelector(".next").addEventListener("click", function(){
+//     for(let i = 0; i<images.length; i++){
+//         document.querySelector(".item").innerHTML = ""
+//         console.log(images)
+//         document.querySelector(".item").innerHTML = `<img src="./${images[i].image}" alt=""></img>`
+
+//         let div = document.createElement("div")
+//         div.classList.add("position-absolute", "bottom-50", "text-right", "color-white", "padding-text")
+
+//         let titolo = `<h2 id="title">${images[i].title}</h2>` 
+//         let text = `<h5 id="text">${images[i].text}</h5>` 
+
+//         div.innerHTML = titolo + text
+        
+//         item.appendChild(div)
+
+//     }
+// })
+
+
